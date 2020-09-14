50927b3cad3cc6f56bf4cdc06b79333185239dcf
NAksenova-ki17 <70371566+NAksenova-ki17@users.noreply.github.com>
2020-09-14 17:54:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class ChatConnection(sockjs.tornado.SockJSConnection):
 
     def on_open(self, info):
         # Send that someone joined
-        self.broadcast(self.participants, "Незнакомец подключидся.")
+        self.broadcast(self.participants, "Незнакомец подключился.")
 
         # Add client to the clients list
         self.participants.add(self)
